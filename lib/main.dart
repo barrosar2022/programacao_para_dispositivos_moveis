0de54e94863a263e5a0b3822cdcb56636c4695a1
anderson barros <anderson.barros@ifpi.edu.br>
2023-04-02 18:10:33 -0300

    Versão 08

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/fundo.jpg'),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
@@ -42,25 +49,26 @@ class HomePage extends StatelessWidget {
                 color: Colors.white,
                 fontWeight: FontWeight.w700),
           ),
-          SizedBox(height: 32),
-          Text(
-            '0',
-            style: TextStyle(
+          const Padding(
+            padding: EdgeInsets.all(40),
+            child: Text(
+              '0',
+              style: TextStyle(
                 fontSize: 100,
                 color: Colors.white,
-                fontWeight: FontWeight.w700),
+              ),
+            ),
           ),
-          SizedBox(height: 32),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children:[
               TextButton(
                 onPressed: decrementar,
                 style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: Size(100, 100),
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
+                    backgroundColor: Colors.white,
+                    fixedSize: Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(24),
                     )
                 ),
@@ -75,12 +83,12 @@ class HomePage extends StatelessWidget {
               TextButton(
                 onPressed: incrementar,
                 style: TextButton.styleFrom(
-                 backgroundColor: Colors.white,
-                 fixedSize: Size(100, 100),
-                 primary: Colors.black,
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(24),
-                 )
+                    backgroundColor: Colors.white,
+                    fixedSize: Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    )
                 ),
                 child: Text(
                   'Entrar',
@@ -92,7 +100,9 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ],
-      ),
+      ),),
+
+
     );
   }
 }
