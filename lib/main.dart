1d1b2cd83ba2c0410c0f732bbd41fa1754e713b4
anderson barros <anderson.barros@ifpi.edu.br>
2023-04-02 21:53:53 -0300

    Versão 10

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
 
   int cont = 0;
 
-  void decrementar(){
-    cont--;
+  void decrementar() {
+    setState(() {
+      cont--;
+    });
   }
 
-  void incrementar(){
-    cont++;
+  void incrementar() {
+    setState(() {
+      cont++;
+    });
   }
 
   @override
@@ -35,73 +45,73 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.black,
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/fundo.jpg'),
-            fit: BoxFit.cover,
-          )
+            image: DecorationImage(
+              image: AssetImage('assets/images/fundo.jpg'),
+              fit: BoxFit.cover,
+            )
         ),
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Pode entrar',
-            style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.w700),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(40),
-            child: Text(
-              '0',
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Pode entrar',
               style: TextStyle(
-                fontSize: 100,
-                color: Colors.white,
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700),
+            ),
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: Text(
+                cont.toString(),
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:[
-              TextButton(
-                onPressed: decrementar,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: Size(100, 100),
-                    primary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrementar,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: Size(100, 100),
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )
+                  ),
+                  child: Text(
+                    'Sair',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24),
+                  ),
                 ),
-                child: Text(
-                  'Sair',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24),
+                SizedBox(width: 32,),
+                TextButton(
+                  onPressed: incrementar,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: Size(100, 100),
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )
+                  ),
+                  child: Text(
+                    'Entrar',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24),
+                  ),
                 ),
-              ),
-              SizedBox(width: 32,),
-              TextButton(
-                onPressed: incrementar,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    fixedSize: Size(100, 100),
-                    primary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    )
-                ),
-                child: Text(
-                  'Entrar',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),),
+              ],
+            ),
+          ],
+        ),),
 
 
     );
